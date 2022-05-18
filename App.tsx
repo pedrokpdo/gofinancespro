313ea4b3a1bfe447f727d0a09f6286703ac54a10
pedrokpdo <pedromotoehm@gmail.com>
2022-05-18 16:27:19 -0300

    fonts personalizadas

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Dashboard } from './src/screens/Dashboard';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme'
+import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
+import { Dashboard } from './src/screens/Dashboard';
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Dashboard/>
     </ThemeProvider>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -6,10 +6,11 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: ${({theme}) => theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.background};
 `
 export const Title = styled.Text`
+    font-family: ${({theme})=> theme.fonts.medium};
     font-size: 24px;
     font-weight: bold;
-    color: ${({theme}) => theme.colors.title};
+    color: ${({ theme }) => theme.colors.title};
 `
