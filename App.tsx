091617c1947849aedf5cf60548ee44662942e14f
pedrokpdo <pedromotoehm@gmail.com>
2022-05-18 15:27:17 -0300

    criando componente com styled-components

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Dashboard } from './src/screens/Dashboard';
 
+
 export default function App() {
   return (
       <Dashboard/>
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Container, Title } from './styles'
+
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Title>mundo</Title>
+        </Container>
+    )
+}
+
