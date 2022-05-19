fae7c31d75e56f64fdec74b58d35444ec81017cf
pedrokpdo <pedromotoehm@gmail.com>
2022-05-19 14:26:49 -0300

    componente botao

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { Button } from "../../components/Forms/Button";
 import { Input } from "../../components/Forms/Input";
-import { Container, Form, Header, Title } from "./styles";
+import { Container, Fields, Form, Header, Title } from "./styles";
 
-export function Register () {
+export function Register() {
     return (
         <Container>
             <Header>
                 <Title>Cadastro</Title>
             </Header>
-        <Form>
-            <Input
-            placeholder="Nome"
-            />
-            <Input
-            placeholder="Preço"
-            />
-        </Form>
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+                    <Input
+                        placeholder="Preço"
+                    />
+                </Fields>
+                <Button title='Enviar' />
+            </Form>
         </Container>
     )
 }
