c3b05067a0e613a203542f088176e88968c17df0
pedrokpdo <pedromotoehm@gmail.com>
2022-05-19 11:44:56 -0300

    componente cartao de transaçao

---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components/native";
+import { Feather } from '@expo/vector-icons'
+import { RFValue } from "react-native-responsive-fontsize";
+
+
+export const Container = styled.View`
+    background-color: ${({ theme }) => theme.colors.shape};
+    border-radius: 5px;
+    padding: 17px 24px;
+`
+export const Title = styled.Text`
+    font-size: ${RFValue(14)}px;
+    font-weight: 400;
+`
+
+export const Amount = styled.Text`
+    font-weight: 400;
+    font-size: ${RFValue(20)}px;
+    margin-top: 2px;
+`
+
+export const Footer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 19px;
+`
+
+export const Category = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    color: ${({ theme }) => theme.colors.text};
+`
+
+export const CategoryName = styled.Text`
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.colors.text};
+    margin-left: 17px;
+`
+
+export const Date = styled.Text`
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.colors.text};
+`
