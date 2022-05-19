2e255b5c7d69a5361ea66a529d68cc241aa29be9
pedrokpdo <pedromotoehm@gmail.com>
2022-05-19 16:23:37 -0300

    Estilização da Interface para Selecionar Categoria

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import theme from './src/global/styles/theme'
 import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 import { Register } from './src/screens/Register';
 import AppLoading from 'expo-app-loading'
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,7 +19,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register/>
+      <CategorySelect/>
     </ThemeProvider>
   );
 }
