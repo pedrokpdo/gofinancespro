3411f9aa683876f5a6788c4418b57d1f60018384
pedrokpdo <pedromotoehm@gmail.com>
2022-05-21 12:41:46 -0300

    Interface de Autenticação

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes'
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
       <StatusBar barStyle='light-content'/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
